c9ea5b3f98e9446617d17bac1b3501de2cca0e01
claze <klaas.schaafsma@gmail.com>
2023-02-21 12:53:15 +0100

    Add files via upload

---
+++ RDP-UDP-Fix.ps1
@@ -0,0 +1,19 @@
+﻿# Quick fix for Microsoft RDP - UDP bug
+# 
+# Sentico ICT Solutions
+#
+$KeyPath = "HKLM:\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services\Client"
+$ValueName = "fClientDisableUDP"
+$ValueData = "1"
+
+try {
+	Get-ItemProperty -Path $KeyPath -Name $ValueName -ErrorAction Stop
+    Write-Host "Key gevonden - fix al doorgevoerd"
+}
+catch [System.Management.Automation.ItemNotFoundException] {
+	Write-Host "Registry Key not found - oops..."
+}
+catch {
+	# Found but not set
+    New-ItemProperty -Path $KeyPath -Name $ValueName -Value $ValueData -PropertyType Dword -Force
+}
